ea620d00b94ae8aa6a9609843d45748619983acd
Aristamo <aljoschaburgmaier@gmail.com>
2019-05-19 22:36:59 +0200

    The message is the same as in the test
    - test back to green

--- src/commonMain/kotlin/sample/Builder.kt
+++ src/commonMain/kotlin/sample/Builder.kt
@@ -5,7 +5,7 @@ class Builder<T: Any?>(val value: T) {
 
     fun isEqualTo(comp: T): Builder<T> {
         if (value != comp) {
-            throw AssertionError()
+            throw AssertionError("Expected <2> to be Equal to <2>, but was <1>")
         }
         return this
     }
