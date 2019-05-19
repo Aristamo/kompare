f11a6d6525d82ae10650f2cade1c2caf282cb1d9
Aristamo <aljoschaburgmaier@gmail.com>
2019-05-19 22:32:17 +0200

    Added second failing test

--- src/commonTest/kotlin/sample/BaseAssertionsTest.kt
+++ src/commonTest/kotlin/sample/BaseAssertionsTest.kt
@@ -9,4 +9,17 @@ class BaseAssertionsTest {
     fun `equality should run trough`() {
         assertThat(1).isEqualTo(1)
     }
+
+    @Test
+    fun `should fail with AssertionError if it is not equal`() {
+        try {
+            assertThat(2).isEqualTo(1)
+        } catch (e: AssertionError) {
+            // Test is succsessfull
+            return
+        } catch (e: Throwable) {
+            throw AssertionError("Wrong Assertion is thrown, expected <${AssertionError::class.simpleName}> but was <${e::class.simpleName}>")
+        }
+        throw AssertionError("No Assertion was thrown")
+    }
 }
