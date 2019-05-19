18964ad8030f31ec84dfe09de0faddc05844e292
Aristamo <aljoschaburgmaier@gmail.com>
2019-05-19 22:36:30 +0200

    Added a failing test for the message

--- src/commonTest/kotlin/sample/BaseAssertionsTest.kt
+++ src/commonTest/kotlin/sample/BaseAssertionsTest.kt
@@ -22,4 +22,13 @@ class BaseAssertionsTest {
         }
         throw AssertionError("No Assertion was thrown")
     }
+
+    @Test
+    fun `message should Equal the same message`() {
+        try {
+            assertThat(2).isEqualTo(1)
+        } catch (e: AssertionError) {
+            assertThat(e.message).isEqualTo("Expected <2> to be Equal to <2>, but was <1>")
+        }
+    }
 }
