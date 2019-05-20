e3b765dd1b68b53a4cd4aa9cddb4168131d2c749
Aristamo <aljoschaburgmaier@gmail.com>
2019-05-20 18:55:58 +0200

    Removed the AssertError Message in the Builder

--- src/commonMain/kotlin/sample/Builder.kt
+++ src/commonMain/kotlin/sample/Builder.kt
@@ -2,10 +2,9 @@ package sample
 
 class Builder<T: Any?>(val value: T) {
 
-
     fun isEqualTo(comp: T): Builder<T> {
         if (value != comp) {
-            throw AssertionError("Expected <2> to be Equal to <2>, but was <1>")
+            throw AssertionError()
         }
         return this
     }
